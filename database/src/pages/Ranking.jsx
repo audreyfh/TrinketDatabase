63889395612fc14392810f6e8c98afd27bb7e941
audreyfh <audreylovesrats@gmail.com>
2025-03-31 10:43:54 -0400

    fixed linking on ranking page

--- database/src/pages/Ranking.jsx
+++ database/src/pages/Ranking.jsx
@@ -6,22 +6,22 @@ function Ranking(){
             <main>
                     <h1>Leaderboard</h1>
                     <p>Ranking changes regularly because I'm indecisive!</p>
-                    <section class="firstplace"><a href="Example-Trinket">
+                    <section class="firstplace"><Link to="Example-Trinket">
                         <div>
                             <h2>First Place: Me as a Rock</h2>
                             <img class="firstplaceimg" src="images/petrock.jpg"/>
                         </div>
-                    </a></section>
+                    </Link></section>
                     <div class="columns">
-                        <div class="ranking"><a href="Example-Trinket">
+                        <div class="ranking"><Link to="Example-Trinket">
                             <img class="squareimg" src="images/macaron.jpg"/>
                             <h3>Second Place: Mia Macaron</h3>
-                        </a></div>
+                        </Link></div>
                     
-                        <div class="ranking"><a href="Example-Trinket">
+                        <div class="ranking"><Link to="Example-Trinket">
                             <img class="squareimg" src="images/candle.jpg"/>
                             <h3>Third Place: Intriguing Candle</h3>
-                        </a></div>
+                        </Link></div>
                     </div>
                 </main>
         </div>
