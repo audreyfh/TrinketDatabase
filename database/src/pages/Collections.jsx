154d66252a1c612a638081fdb51cbd0a7fde0be5
audreyfh <143638226+audreyfh@users.noreply.github.com>
2025-03-31 10:57:00 -0400

    Update Collections.jsx

--- database/src/pages/Collections.jsx
+++ database/src/pages/Collections.jsx
@@ -1,45 +1,45 @@
 import './css/Collections.css'
-
+import {Outlet, Link} from "react-router-dom";
 function Collections(){
     return (
         <div id="content">
             <main>
                     <h2>Collections</h2>
                     <div class="columns">
-                        <section class="col1of2"><a href="./ExampleCollection">
+                        <section class="col1of2"><Link to="./ExampleCollection">
                             <h3>Collectible Trinkets</h3>
                             <img class="collectionimg" src="images/smiski.jpg"/>
-                        </a></section>
-                        <section class="col1of2"><a href="./ExampleCollection">
+                        </Link></section>
+                        <section class="col1of2"><Link to="./ExampleCollection">
                             <h3>Vintage Trinkets</h3>
                             <img class="collectionimg" src="images/monchichi.jpg"/>
-                        </a></section>
-                        <section class="col1of2"><a href="./ExampleCollection">
+                        </Link></section>
+                        <section class="col1of2"><Link to="./ExampleCollection">
                             <h3>Foreign Trinkets</h3>
-                        </a><img class="collectionimg" src="images/mymelo.jpg"/>
-                        </section>
-                        <section class="col1of2"><a href="./ExampleCollection">
+                            <img class="collectionimg" src="images/mymelo.jpg"/>
+                        </Link></section>
+                        <section class="col1of2"><Link to="./ExampleCollection">
                             <h3>"Junk" Trinkets</h3>
-                        </a><img class="collectionimg" src="images/petrock.jpg"/>
+                        </Link><img class="collectionimg" src="images/petrock.jpg"/>
                         </section>
                     </div>
                     <div class="columns">
-                        <section class="col1of2"><a href="./ExampleCollection">
+                        <section class="col1of2"><Link to="./ExampleCollection">
                             <h3>Animal Trinkets</h3>
                             <img class="collectionimg" src="images/catshark.jpg"/>
-                        </a></section>
-                        <section class="col1of2"><a href="./ExampleCollection">
+                        </Link></section>
+                        <section class="col1of2"><Link to="./ExampleCollection">
                             <h3>Cute Trinkets</h3>
                             <img class="collectionimg" src="images/macaron.jpg"/>
-                        </a></section>
-                        <section class="col1of2"><a href="./ExampleCollection">
+                        </Link></section>
+                        <section class="col1of2"><Link to="./ExampleCollection">
                             <h3>Doll Trinkets</h3>
                             <img class="collectionimg" src="images/drac.jpg"/>
-                        </a></section>
-                        <section class="col1of2"><a href="./ExampleCollection">
+                        </Link></section>
+                        <section class="col1of2"><Link to="./ExampleCollection">
                             <h3>Misc. Trinkets</h3>
                             <img class="collectionimg" src="images/bear.jpg"/>
-                        </a></section>
+                        </Link></section>
                     </div>
                 </main>
         </div>
